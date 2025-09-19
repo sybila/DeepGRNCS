f8815a0832320a8bd9c21a7bcc2484ef6a0dc479
llxzy <snowcrash@tuta.io>
2025-09-19 15:33:30 +0200

    Add support for specifying random seed as an argument

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import argparse
+import numpy as np
+import random
+import torch
+
 from src.DeepGRNCS_TF import DeepGRNCS_TF
 from src.DeepGRNCS import DeepGRNCS
 from src.DeepGRNCS_TF_Single import DeepGRNCS_TF_Single
@@ -12,9 +16,22 @@ parser.add_argument('--tf_file', type=str, help='Input TFs information file.')
 parser.add_argument('--gene_file', type=str, help='Input genes information file.')
 parser.add_argument('--save_name', type=str, default='Input the result file.')
 parser.add_argument('--net_number', type=int, default=1, help='Input the number of subpopulations.')
+parser.add_argument('--seed', type=int, help = 'Seed for controlling the randomness of the run')
+
+
+def set_seed(seed: int) -> None:
+    # Have to set seed per library used to make sure it's the same
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
 
 if __name__ == '__main__':
     opt = parser.parse_args()
+
+    if opt.seed is not None:
+        set_seed(opt.seed)
+
     if opt.task == 'DeepGRNCS':
         model = DeepGRNCS(opt)
         model.train_model()
